5e66f6810d58f4498b5bb3b688c03c75e5ce3c61
Peter Mwaura <petermwaura86@gmail.com>
2023-06-19 09:55:52 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prog prints all possible different combinations of these digits
+ * main - prog prints all possible diff combins of these digits
  * Return: Always 0 (Success)
  */
 
@@ -14,7 +14,7 @@ int main(void)
 	{
 		for (tens = '0'; tens <= '9'; tens++)
 		{
-			for (ones = '0'; ones <= '9' ones++)
+			for (ones = '0'; ones <= '9'; ones++)
 			{
 				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 				{
